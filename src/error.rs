c453e8faf774412e54c10cc1674443821470fc12
Peter Farr <Peter@PrismaPhonic.com>
2020-04-29 19:50:25 -0700

    Added owned error types so we could get rid of failure crate.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

---
+++ src/error.rs
@@ -0,0 +1,44 @@
+use reqwest::header::InvalidHeaderValue;
+
+#[derive(Debug)]
+pub enum Error {
+    ReqwestError(reqwest::Error),
+    IoError(std::io::Error),
+    InvalidHeaderValue(InvalidHeaderValue),
+
+    // Remove this. For now - to make things work.
+    StringError(String)
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        // TODO: Make this not shitty.
+        write!(f, "got an error I guess")
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        // TODO: Make this not shitty.
+        None
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(err: reqwest::Error) -> Self {
+        Error::ReqwestError(err)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::IoError(err)
+    }
+}
+
+impl From<InvalidHeaderValue> for Error {
+    fn from(err: InvalidHeaderValue) -> Self {
+        Error::InvalidHeaderValue(err)
+    }
+}
